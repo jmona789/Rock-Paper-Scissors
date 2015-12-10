f7688f4031ad61c2ab096c0903ad10fea478ac46
jmona789 <jmona789@gmail.com>
2015-12-10 13:37:15 -0500

    added animations

--- js/script.js
+++ js/script.js
@@ -5,13 +5,56 @@ $(document).ready(function() {
   var computerWins = 0;
   var playerWins = 0;
   var round = 0;
+  $("#paper").hide();
+  $("#scissors").hide();
+  $(".animation").hide()
+
+  setTimeout(entrancePaper, 300);
+  function entrancePaper(){
+    $("#paper").show();
+  }
+
+  setTimeout(entranceScissors, 600);
+  function entranceScissors(){
+    $("#scissors").show();
+  }
+
+  setTimeout(tossing, 1000);
+  function tossing(){
+    $("#scissors").toggleClass("bigEntrance tossing");
+    $("#rock").toggleClass("bigEntrance tossing");
+    $("#paper").toggleClass("bigEntrance tossing");
+  }
+
+  //Changes animation on mouse enter and changes back on mouse out
+  $(".shown").on("mouseenter", function(){
+    $(this).toggleClass("tossing")
+  });
+  $(".shown").on("mouseleave", function(){
+    $(this).toggleClass("tossing")
+  });
 
   function computerTurn(ranNum){
     computerChoice = rps[ranNum];
     return computerChoice;
   }
 
-  $("a").on("click", function runGame(){
+  function animateOut(){
+    $("#rock").hide();
+    $("#scissors").hide();
+    $(".animation").show();
+    $(".rpsIcons").fadeOut("slow");
+    animateIn();
+  }
+
+  function animateIn(){
+    $("#rock").delay(700).fadeIn("slow");
+    $("#paper").delay(100).fadeIn("slow");
+    $("#scissors").delay(700).fadeIn("slow");
+  }
+
+  $("a").on("click", function(){
+    animateOut();
     var ranNum = Math.floor(Math.random() * rps.length);
     playerChoice = $(this).attr("id");
     computerTurn(ranNum);
@@ -44,16 +87,14 @@ $(document).ready(function() {
             playerWins++;
           }
         }
-      }
-    else{
+      }else{
+        $("a").off();
       if (playerWins < computerWins){
         console.log("Game Over, Player Loses")
-      }
-      else if(playerWins === computerWins){
-        console.log("Game Over, Tie")
-      }
-      else{
-        console.log("Game Over, Player Wins")
+      }else if(playerWins === computerWins){
+        console.log("Game Over, Tie");
+      }else{
+        console.log("Game Over, Player Wins");
       }
     }
   });
