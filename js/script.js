b79f89bee644c3c022611fe40b700fd355a00055
jmona789 <jmona789@gmail.com>
2015-12-12 13:49:47 -0500

    fixed timings for animations, and added gameover function to fix bug

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 $(document).ready(function() {
-  rps = ["rock", "paper", "scissors"];
+  rps = ["Rock", "Paper", "Scissors"];
   var playerChoice;
   var computerChoice;
   var computerWins = 0;
   var playerWins = 0;
-  var round = 0;
-  $("#paper").hide();
-  $("#scissors").hide();
+  var round = 1;
+  $("#paperIcon").hide();
+  $("#scissorsIcon").hide();
   $(".animation").hide();
+  $(".playerChoice").hide();
+  $(".choiceText").hide();
+  $(".winLoseText").hide();
+  $("#roundNum").html("Round: "+round);
 
   setTimeout(entrancePaper, 300);
   function entrancePaper(){
-    $("#paper").show();
+    $("#paperIcon").show();
   }
 
   setTimeout(entranceScissors, 600);
   function entranceScissors(){
-    $("#scissors").show();
+    $("#scissorsIcon").show();
   }
 
   setTimeout(tossing, 1000);
   function tossing(){
-    $("#scissors").toggleClass("bigEntrance tossing");
-    $("#rock").toggleClass("bigEntrance tossing");
-    $("#paper").toggleClass("bigEntrance tossing");
+    $("#scissorsIcon").toggleClass("bigEntrance tossing");
+    $("#rockIcon").toggleClass("bigEntrance tossing");
+    $("#paperIcon").toggleClass("bigEntrance tossing");
   }
 
   //Changes animation on mouse enter and changes back on mouse out
@@ -40,62 +44,103 @@ $(document).ready(function() {
   }
 
   function animateOut(){
-    $("#rock").hide();
-    $("#scissors").hide();
+    $("#rockIcon").hide();
+    $("#scissorsIcon").hide();
     $(".animation").show();
     $(".rpsIcons").fadeOut("slow");
+  }
+
+  function animateChoices(playerChoice, computerChoice){
+    playerChoiceId = "#playerChoice"+playerChoice;
+    computerChoiceId = "#computerChoice"+computerChoice;
+    $(playerChoiceId).delay(500).show(0);
+    $(".choiceText").delay(500).fadeIn("slow");
+    $(playerChoiceId).delay(1600).fadeOut("slow");
+    $(".choiceText").delay(1000).fadeOut("slow");
+    $(computerChoiceId).delay(500).show(0);
+    $(computerChoiceId).delay(1600).fadeOut("slow");
     animateIn();
   }
 
   function animateIn(){
-    $("#rock").delay(700).fadeIn("slow");
-    $("#paper").delay(100).fadeIn("slow");
-    $("#scissors").delay(700).fadeIn("slow");
+    $("#rockIcon").delay(2700).fadeIn("slow");
+    $("#paperIcon").delay(2100).fadeIn("slow");
+    $("#scissorsIcon").delay(2700).fadeIn("slow");
   }
 
   $("a").on("click", function(){
-    animateOut();
-    var ranNum = Math.floor(Math.random() * rps.length);
     playerChoice = $(this).attr("id");
-    computerTurn(ranNum);
-    round ++;
-      if (round <= 5){
-        if (playerChoice === computerChoice){
-          console.log("tie");
-        }else if(playerChoice === "rock"){
-          if (computerChoice ==="paper"){
-            computerWins ++;
-            console.log("player Loses");
-          }else{
-            console.log("player Wins");
-            playerWins++;
-          }
-        }else if(playerChoice === "paper"){
-          if(computerChoice === "scissors"){
-            computerWins ++;
-            console.log("player Loses");
-          }else{
-            console.log("player Wins");
-            playerWins++;
-          }
-        }else if(playerChoice === "scissors"){
-          if(computerChoice === "paper"){
-            console.log("player Loses");
-            computerWins ++;
-          }else{
-            console.log("player Wins");
-            playerWins++;
-          }
+    var ranNum = Math.floor(Math.random() * rps.length);
+    computerchoice = computerTurn(ranNum);
+    animateOut();
+    animateChoices(playerChoice, computerChoice);
+    if (round <= 5){
+      round ++;
+      $("#roundNum").html("Round: "+round);
+      if (playerChoice === computerChoice){
+        round --;
+        $("#roundNum").html("Round: "+round);
+        $(".winLoseText").html("Tie, try that round again!").delay(500).fadeIn("slow");
+        $(".winLoseText").delay(900).fadeOut("slow");
+        GameOverCheck();
+      }else if(playerChoice === "Rock"){
+        if (computerChoice === "Paper"){
+          computerWins ++;
+          $(".winLoseText").html("Computer Wins This Round!").delay(500).fadeIn("slow");
+          $(".winLoseText").delay(900).fadeOut("slow");
+          GameOverCheck();
+        }else{
+          playerWins++;
+          $(".winLoseText").html("You Win This Round!").delay(500).fadeIn("slow");
+          $(".winLoseText").delay(900).fadeOut("slow");
+          GameOverCheck();
         }
-      }else{
-        $("a").off();
-      if (playerWins < computerWins){
-        console.log("Game Over, Player Loses")
-      }else if(playerWins === computerWins){
-        console.log("Game Over, Tie");
-      }else{
-        console.log("Game Over, Player Wins");
+      }else if(playerChoice === "Paper"){
+        if(computerChoice === "Scissors"){
+          computerWins ++;
+          $(".winLoseText").html("Computer Wins This Round!").delay(500).fadeIn("slow");
+          $(".winLoseText").delay(900).fadeOut("slow");
+          GameOverCheck();
+        }else{
+          playerWins++;
+          $(".winLoseText").html("You Win This Round!").delay(500).fadeIn("slow");
+          $(".winLoseText").delay(900).fadeOut("slow");
+          GameOverCheck();
+        }
+      }else if(playerChoice === "Scissors"){
+        if(computerChoice === "Rock"){
+          computerWins ++;
+          $(".winLoseText").html("Computer Wins This Round!").delay(500).fadeIn("slow");
+          $(".winLoseText").delay(900).fadeOut("slow");
+          GameOverCheck();
+        }else{
+          playerWins++;
+          $(".winLoseText").html("You Win This Round!").delay(500).fadeIn("slow");
+          $(".winLoseText").delay(900).fadeOut("slow");
+          GameOverCheck();
+        }
       }
     }
+    $("#playerScore").html(playerWins);
+    $("#computerScore").html(computerWins);
   });
+  
+  function GameOverCheck(){
+    if (round === 6){
+    GameOver();
+    }
+  }
+
+  function GameOver(){
+  round = "Game Over";
+  $("#roundNum").html("Round: "+round);
+  $("a").off();
+    if (playerWins < computerWins){
+      console.log("Game Over, Player Loses")
+    }else if(playerWins === computerWins){
+      console.log("Game Over, Tie");
+    }else{
+      console.log("Game Over, Player Wins");
+    }
+  }
 });
